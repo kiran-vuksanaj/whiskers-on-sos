edd87d860adfea67087872716b32984128843ae9
Coby Sontag <TarzanRoboticsMan@users.noreply.github.com>
2019-10-25 10:28:10 -0400

    Fixed ipad workflow

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def homePage():
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
 
 connector.commit() # save changes
 connector.close()  # close database
