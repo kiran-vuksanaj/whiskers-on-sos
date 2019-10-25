d427dcc19a9b1ff01767f1191048484ecfa31783
Ian Fried <ifried00@stuy.edu>
2019-10-24 22:03:54 -0400

    finished all functions; they need to be checked by others

--- utl/db.py
+++ utl/db.py
@@ -1,7 +1,7 @@
 #Hannah Fried
 #SoftDev
 #Helper fxns for Smallpox Stories
-#Oct 21 2019
+#Oct 21 2019 (creation date)
 import sqlite3   #enable control of an sqlite database
 
 
@@ -13,6 +13,7 @@ curse.execute("CREATE TABLE IF NOT EXISTS stories(Title TEXT UNIQUE PRIMARY KEY,
 def uniqueTitle(title):#return a boolean for whether or not a given title matches one already in the table.
     repeats = curse.execute("SELECT Title FROM stories;")
     for titleS in repeats:
+        #print repeats
         if (names[0] == title):
             return False
     return True
@@ -20,6 +21,7 @@ def uniqueTitle(title):#return a boolean for whether or not a given title matche
 def uniqueUsername(username): #return a boolean for whether or not a given username matches one already in the table
     repeats = curse.execute("SELECT Username FROM users;")
     for names in repeats:
+        #print repeats
         if (names[0] == username):
             return False
     return True
@@ -32,58 +34,38 @@ def addEntry(title, entry, author):# add a row to the stories database, with the
 
 def authenticate(username, password):#return true if the username/password combo exists within the database, otherwise return false.
     givenUser = (username, password)
-    accountTuple = curse.execute("SELECT Password FROM departments WHERE Username = %s;" % username)
+    accountTuple = curse.execute("SELECT Password FROM users WHERE Username = %s;" % username)
     for accounts in accountTuple:
+        #print accountTuple
         if (accounts != password):
             return False
         else:
             return True
-    #I must change return values to case specific errors at some point
+    #Maybe change return values to case specific errors sometime? Up in the air at the time of writing.
 
-def getContributedStories(username):
-    contributedStories = []
-    return contributedStories
-
-def getFullStory(title):
+def getFullStory(title):#return a list of every entry associated with a story within a list for the story
     textEntries = []
+    storyTuple = curse.execute("SELECT Entries FROM stories WHERE Title = %s;" % title)
+    #print storyTuple
+    for currentStory in storyTuple:
+        textEntries.append(currentStory)
     return textEntries
 
-def getOtherStories(username):
+def getContributedStories(username):#return a set of the titles of every story contributed to by an author
+    contributedStories = []
+    titleTuple = curse.execute("SELECT Titles FROM stories WHERE Author = %s;" % username)
+    #print titleTuple
+    for currentTitle in titleTuple:
+        contributedStories.append(currentTitle)
+    return set(contributedStories)
+
+def getOtherStories(username):#return a set of the titles of every NOT story contributed to by an author
     notContributedStories = []
-    return notContributedStories
+    titlesTuple = curse.execute("SELECT Titles FROM stories WHERE Author != %s;" % username)
+    #print titlesTuple
+    for currentTitle in titlesTuple:
+        notContributedStories.append(currentTitle)
+    return set(notContributedStories)
 
 connector.commit() #save changes
 connector.close()  #close database
-#Create new table
-#c.execute("CREATE TABLE IF NOT EXISTS stu_avg(id integer, avg real);")
-
-#Find each student
-#kidList = c.execute("SELECT name, id FROM students;")
-
-#courseList = c.execute("SELECT mark, id FROM courses;")
-
-#gradeTot = 0
-#numClasses = 0
-
-#
-# for kid in kidList: #For each student
-#     print(kid)
-#     currentKid=kid[1]
-#     for uhClass in courseList: #Check each course
-#         currentClass = uhClass[1]
-#         if currentClass == currentKid: #To see if it's their grade
-#             numClasses+=1
-#             gradeTot+=int(uhClass[0]) #Add to average
-#     c.execute("INSERT INTO stu_avg VALUES("+str(currentKid)+","+str(gradeTot/numClasses)+");")
-#     print(kid[0]+", id "+str(kid[1])+": average "+str(gradeTot/numClasses))
-#     gradeTot = 0
-#     numClasses = 0 #reset counters
-# #q = "yadddad {}".format(Hello)
-# def addCourse(code, mark, myid):
-#          c.execute("INSERT INTO courses VALUES("+code+","+mark+","+myid+");")
-#
-#
-# #==========================================================
-#
-# db.commit() #save changes
-# db.close()  #close database
