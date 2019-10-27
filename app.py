3f9f8d18ad33d4120078acf607a91267dd592ee8
Coby Sontag <TarzanRoboticsMan@users.noreply.github.com>
2019-10-26 21:17:04 -0400

    IP style is back

--- app.py
+++ app.py
@@ -115,8 +115,7 @@ def authenticateRegister():
 
 if __name__ == "__main__":
     app.debug = True
-    ##app.run(host='0.0.0.0', port= 80)
-    app.run()
+    app.run(host='0.0.0.0')
 
 connector.commit() # save changes
 connector.close()  # close database
