72a1c78be539a0b4f9135a0dd851d644db2c59cb
Coby Sontag <TarzanRoboticsMan@users.noreply.github.com>
2019-10-27 20:43:37 -0400

    better debug

--- app.py
+++ app.py
@@ -76,7 +76,9 @@ def browse():
     if 'username' not in session:
         return redirect("/login")
     username=session['username']
-    print("\nUsername being fed to getOtherStories: "+username+"\ngetOtherStories(cursor,username): "+db.getOtherStories(cursor, username)[0]+"\n")
+    print("\nUsername being fed to getOtherStories: "+username+"\n")
+    for title in db.getOtherStories(cursor, username):
+        print(title+"\n")
     return render_template("browse.html", user=username, stories=db.getOtherStories(cursor, username))
 
 # Contribution page
